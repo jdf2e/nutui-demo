c36b174f93136c33e85409a6916a8e75bbefeb4c
shenqistart <kevinwangyue@126.com>
2020-04-29 16:39:33 +0800

    test 20200429

--- base/src/main.js
+++ base/src/main.js
@@ -7,11 +7,19 @@ import App from './App.vue'
 
 // NutUI.install(Vue);
 
-
-import { Button, Toast, Infiniteloading, Swiper, Switch } from '@nutui/nutui';
-console.log(Button, Toast, Infiniteloading, Swiper, Switch)
+import { Button, Toast, Infiniteloading, Swiper, Switch, Qart, LuckDraw, Signature, CircleProgress, SideNavBar, Drag, Cell, SubSideNavBar, SideNavBarItem } from '@nutui/nutui';
+console.log(Button, Toast, Infiniteloading, Swiper, Switch, Qart, LuckDraw, Signature, CircleProgress, SideNavBar, Drag, Cell)
 Button.install(Vue);
 Switch.install(Vue);
+Qart.install(Vue);
+LuckDraw.install(Vue);
+Signature.install(Vue);
+CircleProgress.install(Vue);
+SideNavBar.install(Vue);
+Drag.install(Vue);
+Cell.install(Vue);
+SubSideNavBar.install(Vue);
+SideNavBarItem.install(Vue);
 
 import "./assets/rem";
 
