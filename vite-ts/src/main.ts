b874474ae5a3cc31e6bd11708f98204ec7ae6119
richard1015 <51844712@qq.com>
2021-03-04 21:16:42 +0800

    feat: beta.3 support tree shaking

--- vite-ts/src/main.ts
+++ vite-ts/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import NutUI from '@nutui/nutui'
+import {Button} from '@nutui/nutui/dist/nutui.es.js'
+console.log(Button)
 import '@nutui/nutui/dist/style.css';
-createApp(App).use(NutUI).mount('#app')
+createApp(App).use(Button).mount('#app')
