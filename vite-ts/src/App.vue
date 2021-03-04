5d0d38ad21f5447f278cb063fd46cecef66e26bb
richard1015 <51844712@qq.com>
2021-03-04 11:06:05 +0800

    feat: vite ts demo

--- vite-ts/src/App.vue
+++ vite-ts/src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <nut-button>Button 3.0</nut-button>
+  <nut-button type="primary">主要按钮</nut-button>
+  <nut-button type="info">信息按钮</nut-button>
+  <nut-button type="default">默认按钮</nut-button>
+  <nut-button type="danger">危险按钮</nut-button>
+  <nut-button type="warning">警告按钮</nut-button>
+  <nut-button type="success">成功按钮</nut-button>
+  <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
 <script lang="ts">
--- vite-ts/src/main.ts
+++ vite-ts/src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import NutUI from '@nutui/nutui'
+import '@nutui/nutui/dist/style.css';
+createApp(App).use(NutUI).mount('#app')
