91292a515038b5479a0f29730ddd0aa20bea19ff
richard1015 <51844712@qq.com>
2021-03-15 11:32:45 +0800

    test: tabbar

--- vite-ts/src/App.vue
+++ vite-ts/src/App.vue
@@ -1,25 +1,54 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <nut-button>Button 3.0</nut-button>
-  <nut-button type="primary">主要按钮</nut-button>
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+
+  <nut-button type="primary" @click="testNavBar">主要按钮</nut-button>
   <nut-button type="info">信息按钮</nut-button>
   <nut-button type="default">默认按钮</nut-button>
   <nut-button type="danger">危险按钮</nut-button>
   <nut-button type="warning">警告按钮</nut-button>
   <nut-button type="success">成功按钮</nut-button>
-  <nut-cell title="123123213312">123123</nut-cell>
-  <nut-icon name="dongdong"></nut-icon>
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+
+  <nut-tabbar :bottom="true" :show="active" @tab-switch="tabSwitch">
+    <nut-tabbar-item
+      v-for="item in tests"
+      :tab-title="item.title"
+      :href="item.href"
+      :icon="item.icon"
+      :num="item.num"
+    ></nut-tabbar-item>
+  </nut-tabbar>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref, reactive } from "vue";
 import HelloWorld from "./components/HelloWorld.vue";
-
 export default defineComponent({
   name: "App",
   components: {
     HelloWorld,
+  },
+  setup() {
+    const tests = reactive([
+      { title: "首页", icon: "home", href: "", num: 0 },
+      { title: "分类", icon: "category", href: "https://m.jd.com", num: 1 },
+    ]);
+    const active = ref(0);
+    function tabSwitch(item: object, index: number) {
+      console.log(item, index);
+    }
+    function testNavBar() {
+      tests.push({ title: "发现", icon: "find", href: "", num: 0 });
+      tests[0].href = "https://m.jd.com";
+      active.value = 2;
+      tests[0].num = 3;
+    }
+    return {
+      testNavBar,
+      tabSwitch,
+      tests,
+      active,
+    };
   },
 });
 </script>
@@ -32,5 +61,6 @@ export default defineComponent({
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  padding-bottom: 100px;
 }
 </style>
--- vite-ts/src/main.ts
+++ vite-ts/src/main.ts
@@ -1,5 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import { Button, Cell, Icon } from "@nutui/nutui";
+import { Button, Cell, Icon, Tabbar, Toast } from "@nutui/nutui";
 import "@nutui/nutui/dist/style.css";
-createApp(App).use(Button).use(Cell).use(Icon).mount("#app");
+createApp(App)
+  .use(Button)
+  .use(Cell)
+  .use(Icon)
+  .use(Tabbar)
+  .use(Toast)
+  .mount("#app");
