929c0f9c86fb86ee1e40ae63241f42609a67f7a3
richard1015 <51844712@qq.com>
2021-07-22 15:49:53 +0800

    taro3

---
+++ taro3/src/app.ts
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+import { Button, Icon, Toast } from '@nutui/nutui-taro';
+
+import '@nutui/nutui-taro/dist/styles/themes/default.scss';
+import './app.scss'
+
+const App = createApp({
+  onShow (options) {},
+  // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
+})
+
+App.use(Button).use(Toast).use(Icon)
+
+export default App
