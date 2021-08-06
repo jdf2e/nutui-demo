dfcc3a4ec0ee1527dca0c91780e6838e3cd5fb1a
richard1015 <51844712@qq.com>
2021-08-06 18:46:37 +0800

    chore: taro 3.3.2

--- taro/src/app.ts
+++ taro/src/app.ts
@@ -1,7 +1,10 @@
 import { createApp } from 'vue';
 import './app.scss';
 import NutUI from '@nutui/nutui-taro';
-import '@nutui/nutui-taro/dist/style.css';
+// import '@nutui/nutui-taro/dist/style.css';
+
+import '@nutui/nutui-taro/dist/styles/themes/default.scss';
+
 const App = createApp({
   onShow(options) {}
   // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
