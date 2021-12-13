76cef1aafd1570d1dea2b43bc6b9139daab22601
dongji136 <dongji136@jd.com>
2021-12-13 14:26:43 +0800

    upd: add cascader test

--- vite/src/main.js
+++ vite/src/main.js
@@ -1,6 +1,30 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import { Button, Cell, Icon,FixedNav,OverLay } from "@nutui/nutui";
+import {
+  Button,
+  Cell,
+  Icon,
+  FixedNav,
+  OverLay,
+  Form,
+  FormItem,
+  Cascader,
+  Popup,
+  Tabs,
+  TabPane,
+} from "@nutui/nutui";
 import "@nutui/nutui/dist/style.css";
-import './assets/custom_font/iconfont.css';
-createApp(App).use(Button).use(Cell).use(Icon).use(FixedNav).use(OverLay).mount("#app");
+import "./assets/custom_font/iconfont.css";
+createApp(App)
+  .use(Button)
+  .use(Cell)
+  .use(Icon)
+  .use(FixedNav)
+  .use(OverLay)
+  .use(Form)
+  .use(FormItem)
+  .use(Cascader)
+  .use(Tabs)
+  .use(Popup)
+  .use(TabPane)
+  .mount("#app");
