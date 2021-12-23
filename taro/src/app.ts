70baa129b94b9b89cf62b6cf8bdc8610517d9f30
richard1015 <51844712@qq.com>
2021-12-23 10:22:30 +0800

    upd: 3.1.14 nutui-taro test

--- taro/src/app.ts
+++ taro/src/app.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import './app.scss';
-import {   Avatar,Button,Cell,CellGroup,Icon,Price,OverLay,Divider,Layout,Col,Row,ImagePreview,Swipe,ActionSheet,BackTop,Collapse,CollapseItem,Drag,Dialog,InfiniteLoading,Notify,Range,Popup,Steps,Step,Swiper,SwiperItem,Switch,Toast,Progress,CircleProgress,NoticeBar,SearchBar,Navbar,FixedNav,Menu,MenuItem,Tabbar,TabbarItem,Elevator,Pagination,Tabs,TabPane,Calendar,Checkbox,CheckboxGroup,DatePicker,InputNumber,Input,Radio,RadioGroup,Rate,Picker,ShortPassword,TextArea,Uploader,CountUp,NumberKeyboard,CountDown,Badge,Tag,Popover,Form,FormItem,Address,Barrage,Signature,TimeSelect,TimePannel,TimeDetail,Sku } from '@nutui/nutui-taro';
+import {   Avatar,Button,Cell,CellGroup,Icon,Price,OverLay,Divider,Layout,Col,Row,Swipe,ActionSheet,BackTop,Collapse,CollapseItem,Drag,Dialog,InfiniteLoading,Notify,Range,Popup,Steps,Step,Swiper,SwiperItem,Switch,Toast,Progress,CircleProgress,NoticeBar,SearchBar,Empty,Navbar,FixedNav,Menu,MenuItem,Tabbar,TabbarItem,Elevator,Pagination,Tabs,TabPane,Indicator,Grid,GridItem,Calendar,Checkbox,CheckboxGroup,DatePicker,InputNumber,Input,Radio,RadioGroup,Rate,Picker,ShortPassword,TextArea,Uploader,CountUp,NumberKeyboard,CountDown,Badge,Tag,Popover,Form,FormItem,Skeleton,Cascader,Address,Barrage,Signature,TimeSelect,TimePannel,TimeDetail,Sku,Card,Table } from '@nutui/nutui-taro';
 // import '@nutui/nutui-taro/dist/style.css';
 
 // import '@nutui/nutui-taro/dist/styles/themes/default.scss';
@@ -8,7 +8,7 @@ import '@nutui/nutui-taro/dist/style.css';
 
 //这里添加用到的 nutui 组件
 const nutuiComponents = [
-  Avatar,Button,Cell,CellGroup,Icon,Price,OverLay,Divider,Layout,Col,Row,ImagePreview,Swipe,ActionSheet,BackTop,Collapse,CollapseItem,Drag,Dialog,InfiniteLoading,Notify,Range,Popup,Steps,Step,Swiper,SwiperItem,Switch,Toast,Progress,CircleProgress,NoticeBar,SearchBar,Navbar,FixedNav,Menu,MenuItem,Tabbar,TabbarItem,Elevator,Pagination,Tabs,TabPane,Calendar,Checkbox,CheckboxGroup,DatePicker,InputNumber,Input,Radio,RadioGroup,Rate,Picker,ShortPassword,TextArea,Uploader,CountUp,NumberKeyboard,CountDown,Badge,Tag,Popover,Form,FormItem,Address,Barrage,Signature,TimeSelect,TimePannel,TimeDetail,Sku 
+  Avatar,Button,Cell,CellGroup,Icon,Price,OverLay,Divider,Layout,Col,Row,Swipe,ActionSheet,BackTop,Collapse,CollapseItem,Drag,Dialog,InfiniteLoading,Notify,Range,Popup,Steps,Step,Swiper,SwiperItem,Switch,Toast,Progress,CircleProgress,NoticeBar,SearchBar,Empty,Navbar,FixedNav,Menu,MenuItem,Tabbar,TabbarItem,Elevator,Pagination,Tabs,TabPane,Indicator,Grid,GridItem,Calendar,Checkbox,CheckboxGroup,DatePicker,InputNumber,Input,Radio,RadioGroup,Rate,Picker,ShortPassword,TextArea,Uploader,CountUp,NumberKeyboard,CountDown,Badge,Tag,Popover,Form,FormItem,Skeleton,Cascader,Address,Barrage,Signature,TimeSelect,TimePannel,TimeDetail,Sku,Card,Table
 ];
 
 const App = createApp({
--- taro/src/dentry/pages/form/index.vue
+++ taro/src/dentry/pages/form/index.vue
@@ -14,24 +14,74 @@
       <nut-form-item label="地址">
         <input class="nut-input-text" placeholder="请输入地址" type="text" />
       </nut-form-item>
+      <nut-form-item label="备注">
+        <nut-textarea placeholder="请输入备注" type="text" />
+      </nut-form-item>
+    </nut-form>
+    <h2>动态表单</h2>
+    <nut-form :model-value="dynamicForm.state" ref="dynamicRefForm">
+      <nut-form-item label="姓名" prop="name" required :rules="[{ required: true, message: '请填写姓名' }]">
+        <input class="nut-input-text" v-model="dynamicForm.state.name" placeholder="请输入姓名" type="text" />
+      </nut-form-item>
+      <nut-form-item
+        :label="'联系方式' + index"
+        :prop="'tels.' + index + '.value'"
+        required
+        :rules="[{ required: true, message: '请填写联系方式' + index }]"
+        :key="item.key"
+        v-for="(item, index) in dynamicForm.state.tels"
+      >
+        <input class="nut-input-text" v-model="item.value" :placeholder="'请输入联系方式' + index" type="text" />
+      </nut-form-item>
+      <nut-cell>
+        <nut-button size="small" style="margin-right: 10px" @click="dynamicForm.methods.add">添加</nut-button>
+        <nut-button size="small" style="margin-right: 10px" @click="dynamicForm.methods.remove">删除</nut-button>
+        <nut-button type="primary" size="small" @click="dynamicForm.methods.submit">提交</nut-button>
+      </nut-cell>
     </nut-form>
     <h2>表单校验</h2>
     <nut-form :model-value="formData" ref="ruleForm">
       <nut-form-item label="姓名" prop="name" required :rules="[{ required: true, message: '请填写姓名' }]">
-        <input class="nut-input-text" v-model="formData.name" placeholder="请输入姓名" type="text" />
+        <input
+          class="nut-input-text"
+          @blur="customBlurValidate('name')"
+          v-model="formData.name"
+          placeholder="请输入姓名，blur 事件校验"
+          type="text"
+        />
       </nut-form-item>
-      <nut-form-item label="年龄" prop="age" required :rules="[
+      <nut-form-item
+        label="年龄"
+        prop="age"
+        required
+        :rules="[
           { required: true, message: '请填写年龄' },
           { validator: customValidator, message: '必须输入数字' },
           { regex: /^(\d{1,2}|1\d{2}|200)$/, message: '必须输入0-200区间' }
-        ]">
-        <input class="nut-input-text" v-model="formData.age" placeholder="请输入年龄，必须数字且0-200区间" type="text" />
+        ]"
+      >
+        <input
+          class="nut-input-text"
+          v-model="formData.age"
+          placeholder="请输入年龄，必须数字且0-200区间"
+          type="text"
+        />
       </nut-form-item>
-      <nut-form-item label="联系电话" prop="tel" required :rules="[
+      <nut-form-item
+        label="联系电话"
+        prop="tel"
+        required
+        :rules="[
           { required: true, message: '请填写联系电话' },
           { validator: asyncValidator, message: '电话格式不正确' }
-        ]">
-        <input class="nut-input-text" v-model="formData.tel" placeholder="请输入联系电话，异步校验电话格式" type="text" />
+        ]"
+      >
+        <input
+          class="nut-input-text"
+          v-model="formData.tel"
+          placeholder="请输入联系电话，异步校验电话格式"
+          type="text"
+        />
       </nut-form-item>
       <nut-form-item label="地址" prop="address" required :rules="[{ required: true, message: '请填写地址' }]">
         <input class="nut-input-text" v-model="formData.address" placeholder="请输入地址" type="text" />
@@ -70,12 +120,24 @@
         </nut-uploader>
       </nut-form-item>
       <nut-form-item label="地址">
-        <input class="nut-input-text" v-model="formData2.address" @click="addressModule.methods.show" readonly
-          placeholder="请选择地址" type="text" />
+        <input
+          class="nut-input-text"
+          v-model="formData2.address"
+          @click="addressModule.methods.show"
+          readonly
+          placeholder="请选择地址"
+          type="text"
+        />
         <!-- nut-address -->
-        <nut-address v-model:visible="addressModule.state.show" :province="addressModule.state.province"
-          :city="addressModule.state.city" :country="addressModule.state.country" :town="addressModule.state.town"
-          @change="addressModule.methods.onChange" custom-address-title="请选择所在地区"></nut-address>
+        <nut-address
+          v-model:visible="addressModule.state.show"
+          :province="addressModule.state.province"
+          :city="addressModule.state.city"
+          :country="addressModule.state.country"
+          :town="addressModule.state.town"
+          @change="addressModule.methods.onChange"
+          custom-address-title="请选择所在地区"
+        ></nut-address>
       </nut-form-item>
     </nut-form>
   </div>
@@ -92,6 +154,40 @@ export default {
       tel: '',
       address: ''
     });
+
+    const dynamicRefForm = ref<any>(null);
+    const dynamicForm = {
+      state: reactive({
+        name: '',
+        tels: new Array({
+          key: 1,
+          value: ''
+        })
+      }),
+
+      methods: {
+        submit() {
+          dynamicRefForm.value.validate().then(({ valid, errors }: any) => {
+            if (valid) {
+              console.log('success', dynamicForm);
+            } else {
+              console.log('error submit!!', errors);
+            }
+          });
+        },
+        remove() {
+          dynamicForm.state.tels.splice(dynamicForm.state.tels.length - 1, 1);
+        },
+        add() {
+          let newIndex = dynamicForm.state.tels.length;
+          dynamicForm.state.tels.push({
+            key: Date.now(),
+            value: ''
+          });
+        }
+      }
+    };
+
     const validate = (item: any) => {
       console.log(item);
     };
@@ -175,6 +271,16 @@ export default {
     const reset = () => {
       ruleForm.value.reset();
     };
+    // 失去焦点校验
+    const customBlurValidate = (prop: string) => {
+      ruleForm.value.validate(prop).then(({ valid, errors }: any) => {
+        if (valid) {
+          console.log('success', formData);
+        } else {
+          console.log('error submit!!', errors);
+        }
+      });
+    };
     // 函数校验
     const customValidator = (val: string) => /^\d+$/.test(val);
     // Promise 异步校验
@@ -187,9 +293,22 @@ export default {
         }, 1000);
       });
     };
-    return { ruleForm, formData, validate, customValidator, asyncValidator, submit, reset, formData2, addressModule };
+    return {
+      ruleForm,
+      formData,
+      validate,
+      customValidator,
+      asyncValidator,
+      customBlurValidate,
+      submit,
+      reset,
+      formData2,
+      addressModule,
+      dynamicForm,
+      dynamicRefForm
+    };
   }
-}
+};
 </script>
 
 <style lang="scss" scoped></style>
