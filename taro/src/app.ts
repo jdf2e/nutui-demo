21f92b2af54385546f1f1e04efdef4165fe23164
suzigang <suzigang@jd.com>
2022-01-21 16:07:13 +0800

    feat(sidenavbar): 新增侧边栏导航组件

--- taro/src/app.ts
+++ taro/src/app.ts
@@ -1,6 +1,14 @@
+/*
+ * @Author: your name
+ * @Date: 2022-01-21 15:46:47
+ * @LastEditTime: 2022-01-21 15:58:44
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /nutui-demo/taro/src/app.ts
+ */
 import { createApp } from 'vue';
 import './app.scss';
-import {   Avatar,Button,Cell,CellGroup,Icon,Price,OverLay,Divider,Layout,Col,Row,Swipe,ActionSheet,BackTop,Collapse,CollapseItem,Drag,Dialog,InfiniteLoading,Notify,Range,Popup,Steps,Step,Swiper,SwiperItem,Switch,Toast,Progress,CircleProgress,NoticeBar,SearchBar,Empty,Navbar,FixedNav,Menu,MenuItem,Tabbar,TabbarItem,Elevator,Pagination,Tabs,TabPane,Indicator,Grid,GridItem,Calendar,Checkbox,CheckboxGroup,DatePicker,InputNumber,Input,Radio,RadioGroup,Rate,Picker,ShortPassword,TextArea,Uploader,CountUp,NumberKeyboard,CountDown,Badge,Tag,Popover,Form,FormItem,Skeleton,Cascader,Address,Barrage,Signature,TimeSelect,TimePannel,TimeDetail,Sku,Card,Table } from '@nutui/nutui-taro';
+import {   Avatar,Button,Cell,CellGroup,Icon,Price,OverLay,Divider,Layout,Col,Row,Swipe,ActionSheet,BackTop,Collapse,CollapseItem,Drag,Dialog,InfiniteLoading,Notify,Range,Popup,Steps,Step,Swiper,SwiperItem,Switch,Toast,Progress,CircleProgress,NoticeBar,SearchBar,Empty,Navbar,FixedNav,Menu,MenuItem,Tabbar,TabbarItem,Elevator,Pagination,Tabs,TabPane,Indicator,Grid,GridItem,Calendar,Checkbox,CheckboxGroup,DatePicker,InputNumber,Input,Radio,RadioGroup,Rate,Picker,ShortPassword,TextArea,Uploader,CountUp,NumberKeyboard,CountDown,Badge,Tag,Popover,Form,FormItem,Skeleton,Cascader,Address,Barrage,Signature,TimeSelect,TimePannel,TimeDetail,Sku,Card,Table, SideNavBar, SideNavBarItem, SubSideNavBar } from '@nutui/nutui-taro';
 // import '@nutui/nutui-taro/dist/style.css';
 
 // import '@nutui/nutui-taro/dist/styles/themes/default.scss';
@@ -8,7 +16,7 @@ import '@nutui/nutui-taro/dist/style.css';
 
 //这里添加用到的 nutui 组件
 const nutuiComponents = [
-  Avatar,Button,Cell,CellGroup,Icon,Price,OverLay,Divider,Layout,Col,Row,Swipe,ActionSheet,BackTop,Collapse,CollapseItem,Drag,Dialog,InfiniteLoading,Notify,Range,Popup,Steps,Step,Swiper,SwiperItem,Switch,Toast,Progress,CircleProgress,NoticeBar,SearchBar,Empty,Navbar,FixedNav,Menu,MenuItem,Tabbar,TabbarItem,Elevator,Pagination,Tabs,TabPane,Indicator,Grid,GridItem,Calendar,Checkbox,CheckboxGroup,DatePicker,InputNumber,Input,Radio,RadioGroup,Rate,Picker,ShortPassword,TextArea,Uploader,CountUp,NumberKeyboard,CountDown,Badge,Tag,Popover,Form,FormItem,Skeleton,Cascader,Address,Barrage,Signature,TimeSelect,TimePannel,TimeDetail,Sku,Card,Table
+  Avatar,Button,Cell,CellGroup,Icon,Price,OverLay,Divider,Layout,Col,Row,Swipe,ActionSheet,BackTop,Collapse,CollapseItem,Drag,Dialog,InfiniteLoading,Notify,Range,Popup,Steps,Step,Swiper,SwiperItem,Switch,Toast,Progress,CircleProgress,NoticeBar,SearchBar,Empty,Navbar,FixedNav,Menu,MenuItem,Tabbar,TabbarItem,Elevator,Pagination,Tabs,TabPane,Indicator,Grid,GridItem,Calendar,Checkbox,CheckboxGroup,DatePicker,InputNumber,Input,Radio,RadioGroup,Rate,Picker,ShortPassword,TextArea,Uploader,CountUp,NumberKeyboard,CountDown,Badge,Tag,Popover,Form,FormItem,Skeleton,Cascader,Address,Barrage,Signature,TimeSelect,TimePannel,TimeDetail,Sku,Card,Table,SideNavBar, SubSideNavBar, SideNavBarItem
 ];
 
 const App = createApp({
