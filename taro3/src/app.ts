ba1925188ab391c501895544c073d8d69f18455e
yangxiaolu3 <yangxiaolu3@jd.com>
2022-03-29 12:41:12 +0800

    feat: 新增jsx

--- taro3/src/app.ts
+++ taro3/src/app.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import { Avatar, Button, FixedNav, Icon, Input, OverLay, Toast,Swipe,Cell,Progress } from '@nutui/nutui-taro';
+import { Avatar, Button, FixedNav, Icon, Input, OverLay, Toast,Swipe,Cell,Progress ,Picker} from '@nutui/nutui-taro';
 
 
 
@@ -8,6 +8,6 @@ const App = createApp({
   // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
 })
 
-App.use(Button).use(Input).use(Toast).use(Icon).use(Avatar).use(FixedNav).use(OverLay).use(Swipe).use(Cell).use(Progress)
+App.use(Button).use(Input).use(Toast).use(Icon).use(Avatar).use(FixedNav).use(OverLay).use(Swipe).use(Cell).use(Progress).use(Picker)
 
 export default App
---
+++ taro3/src/pages/index/indexJSX.vue
@@ -0,0 +1,37 @@
+<script>
+import { View } from "@tarojs/components";
+import { defineComponent, ref } from "vue";
+import { Button, Picker, Popup } from "@nutui/nutui-taro";
+export default {
+  setup() {
+    let show = ref(true);
+    let desc = ref("11");
+    let columns = ref([
+      { text: "南京市", value: "NanJing" },
+      { text: "无锡市", value: "WuXi" },
+      { text: "海北藏族自治区", value: "ZangZu" },
+      { text: "北京市", value: "BeiJing" },
+      { text: "连云港市", value: "LianYunGang" },
+      { text: "浙江市", value: "ZheJiang" },
+      { text: "江苏市", value: "JiangSu" },
+    ]);
+
+    return () => (
+      <View>
+        <nut-cell
+          title="请选择城市"
+          desc={desc}
+          click={() => {
+            show = true;
+          }}
+        ></nut-cell>{" "}
+        <nut-picker
+          v-model:visible={show}
+          columns={columns}
+          title="城市选择"
+        ></nut-picker>
+      </View>
+    );
+  },
+};
+</script>
