d4300cd43f4894bf126f485771af67894a9bb355
richard1015 <51844712@qq.com>
2022-03-29 14:05:01 +0800

    upd: vue3  vuecli

--- vue3-vue-cli/src/main.js
+++ vue3-vue-cli/src/main.js
@@ -14,7 +14,7 @@ import {
     NoticeBar,
     Picker
 } from "@nutui/nutui";
-import "@nutui/nutui/dist/style.css";
+// import "@nutui/nutui/dist/style.css";
 
 const app = createApp(App)
 app.use(router).use(Button).use(Price).use(Input).use(Icon).use(Cell).use(Tabs).use(TabPane).use(NoticeBar).use(Picker).mount("#app");
--- vue3-vue-cli/src/views/index.vue
+++ vue3-vue-cli/src/views/index.vue
@@ -157,7 +157,7 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
