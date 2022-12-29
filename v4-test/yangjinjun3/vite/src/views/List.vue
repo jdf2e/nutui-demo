7be655ce9bdbcff4fb645d7dfe11debf736c1064
yangjinjun3 <yangjinjun3@jd.com>
2022-12-29 10:09:59 +0800

    add vite and taro

---
+++ v4-test/yangjinjun3/vite/src/views/List.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Config from '../config.json';
+import { useRouter } from 'vue-router';
+const router = useRouter();
+</script>
+
+<template>
+  <div class="list">
+    <div class="item" v-for="item in Config.list">
+      <nut-cell @click="router.push(`/${item}`)">{{ item }}</nut-cell>
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
