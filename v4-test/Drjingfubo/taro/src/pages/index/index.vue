ab5539699139a3d508468936f2f1d78c3791b810
Drjingfubo <1783194754@qq.com>
2022-12-29 10:53:45 +0800

    feat: add demo

---
+++ v4-test/Drjingfubo/taro/src/pages/index/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <view class="index">
+    <view>
+      <img src="" alt="">
+    </view>
+    {{ msg }}
+    <view class="btn">
+      <nut-button type="primary" @click="handleClick('text', msg2, true)">点我</nut-button>
+    </view>
+    <nut-toast :msg="msg" v-model:visible="show" :type="type" :cover="cover" />
+  </view>
+</template>
+
+<script>
+import { reactive, toRefs } from 'vue';
+export default {
+  name: 'Index',
+  components: {
+    
+  },
+  setup(){
+    const state = reactive({
+      msg: '欢迎使用 NutUI3.0 开发小程序',
+      msg2: '你成功了～',
+      type: 'text',
+      show: false,
+      cover: false
+    });
+
+    const handleClick = (type, msg, cover = false) => {
+      state.show = true;
+      state.msg2 = msg;
+      state.type = type;
+      state.cover = cover;
+    };
+
+    return {
+      ...toRefs(state),
+      handleClick
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.index {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
---
+++ v4-test/Drjingfubo/taro/types/global.d.ts
@@ -0,0 +1,30 @@
+/// <reference types="@tarojs/taro" />
+
+declare module '*.png';
+declare module '*.gif';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.svg';
+declare module '*.css';
+declare module '*.less';
+declare module '*.scss';
+declare module '*.sass';
+declare module '*.styl';
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    TARO_ENV: 'weapp' | 'swan' | 'alipay' | 'h5' | 'rn' | 'tt' | 'quickapp' | 'qq' | 'jd'
+  }
+}
+
+declare module '@tarojs/components' {
+  export * from '@tarojs/components/types/index.vue3'
+}
+declare module '@vue/runtime-core' {
+  export interface GlobalComponents extends JSX.IntrinsicElements {
+    /** Note: Vue 在 runtime 中将 JSX.IntrinsicElements 通过 index signature 重复声明标签
+     * 这会导致插件无法正常跳转类型，可以手动覆盖声明标签活得更好的体验，参考如下：
+     * 'scroll-view': JSX.IntrinsicElements['scroll-view']
+     */
+  }
+}
