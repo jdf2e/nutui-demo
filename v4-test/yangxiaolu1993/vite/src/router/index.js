f3f14f1d1dec63d7466d3b1d8ebf351b5a75f4c1
yangxiaolu3 <yangxiaolu3@jd.com>
2022-12-30 16:25:55 +0800

    feat: taro

--- v4-test/yangxiaolu1993/vite/src/router/index.js
+++ v4-test/yangxiaolu1993/vite/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter,createWebHistory } from "vue-router";
 import HelloWorld from '../components/HelloWorld.vue';
+import Button from '../components/button.vue'
 
 const router = createRouter({
     history:createWebHistory(),
@@ -7,6 +8,10 @@ const router = createRouter({
         {
             path:'/',
             component:HelloWorld,
+        },
+        {
+            path:'/button',
+            component:Button,
         }
     ]
 })
