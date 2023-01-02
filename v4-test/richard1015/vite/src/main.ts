11214ae2e1cb664f930e46d2ff15e6268c296751
richard1015 <51844712@qq.com>
2023-01-02 23:31:38 +0800

    feat: test Locale

--- v4-test/richard1015/vite/src/main.ts
+++ v4-test/richard1015/vite/src/main.ts
@@ -1,6 +1,19 @@
 import { createApp } from 'vue'
 // import NutUI from '@nutui/nutui';
 // import '@nutui/nutui/dist/style.css';
+import NutUI from '@nutui/nutui';
+import us from "@nutui/nutui/dist/packages/locale/lang/en-US";
+import tw from "@nutui/nutui/dist/packages/locale/lang/zh-TW";
+setTimeout(() => {
+    NutUI.Locale.use('en-US', us);
+}, 1000 * 5);
+setTimeout(() => {
+    NutUI.Locale.use('zh-CN');
+}, 1000 * 10);
+setTimeout(() => {
+    NutUI.Locale.use('zh-TW', tw);
+}, 1000 * 15);
+console.log(NutUI);
 import App from './App.vue'
 createApp(App)
     // .use(NutUI)
