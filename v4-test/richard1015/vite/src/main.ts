d76080080275a67c99333541723cecc31b01f749
richard1015 <51844712@qq.com>
2023-01-03 22:03:11 +0800

    test: 国际化测试

--- v4-test/richard1015/vite/src/main.ts
+++ v4-test/richard1015/vite/src/main.ts
@@ -1,19 +1,25 @@
 import { createApp } from 'vue'
 // import NutUI from '@nutui/nutui';
 // import '@nutui/nutui/dist/style.css';
-import NutUI from '@nutui/nutui';
+import { Locale } from '@nutui/nutui';
 import us from "@nutui/nutui/dist/packages/locale/lang/en-US";
 import tw from "@nutui/nutui/dist/packages/locale/lang/zh-TW";
+Locale.merge('zh-CN', {
+    confirm: '关闭',// 将 "确认" 修改为 "关闭"
+    // 修改某个组件内部语言
+    shortpassword: {
+        title: '请输入密码'// 将 "请输入密码" 修改为 "请输入卡密"
+    }
+});
 setTimeout(() => {
-    NutUI.Locale.use('en-US', us);
+    Locale.use('en-US', us);
 }, 1000 * 5);
 setTimeout(() => {
-    NutUI.Locale.use('zh-CN');
+    Locale.use('zh-CN');
 }, 1000 * 10);
 setTimeout(() => {
-    NutUI.Locale.use('zh-TW', tw);
+    Locale.use('zh-TW', tw);
 }, 1000 * 15);
-console.log(NutUI);
 import App from './App.vue'
 createApp(App)
     // .use(NutUI)
