a43d74b206faa79baf844fde59722f05932b5280
richard1015 <51844712@qq.com>
2023-01-07 20:43:37 +0800

    upd: 测试用例提交

--- v4-test/richard1015/taro/components.d.ts
+++ v4-test/richard1015/taro/components.d.ts
@@ -7,8 +7,27 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    NutAddress: typeof import('@nutui/nutui-taro')['Address']
+    NutAvatar: typeof import('@nutui/nutui-taro')['Avatar']
+    NutBadge: typeof import('@nutui/nutui-taro')['Badge']
     NutButton: typeof import('@nutui/nutui-taro')['Button']
+    NutCell: typeof import('@nutui/nutui-taro')['Cell']
+    NutCellGroup: typeof import('@nutui/nutui-taro')['CellGroup']
+    NutCheckbox: typeof import('@nutui/nutui-taro')['Checkbox']
+    NutConfigProvider: typeof import('@nutui/nutui-taro')['ConfigProvider']
+    NutCountdown: typeof import('@nutui/nutui-taro')['Countdown']
+    NutForm: typeof import('@nutui/nutui-taro')['Form']
+    NutFormItem: typeof import('@nutui/nutui-taro')['FormItem']
+    NutInput: typeof import('@nutui/nutui-taro')['Input']
+    NutInputNumber: typeof import('@nutui/nutui-taro')['InputNumber']
+    NutNumberKeyboard: typeof import('@nutui/nutui-taro')['NumberKeyboard']
+    NutPopover: typeof import('@nutui/nutui-taro')['Popover']
+    NutRadio: typeof import('@nutui/nutui-taro')['Radio']
+    NutRadioGroup: typeof import('@nutui/nutui-taro')['RadioGroup']
+    NutRange: typeof import('@nutui/nutui-taro')['Range']
     NutRate: typeof import('@nutui/nutui-taro')['Rate']
+    NutSwitch: typeof import('@nutui/nutui-taro')['Switch']
+    NutTag: typeof import('@nutui/nutui-taro')['Tag']
     NutToast: typeof import('@nutui/nutui-taro')['Toast']
     NutUploader: typeof import('@nutui/nutui-taro')['Uploader']
   }
--- v4-test/richard1015/taro/src/app.config.ts
+++ v4-test/richard1015/taro/src/app.config.ts
@@ -1,7 +1,14 @@
 export default defineAppConfig({
   pages: [
+    'pages/popover/index',
+    'pages/tag/index',
+    'pages/badge/index',
+    'pages/countdown/index',
+    'pages/form/index',
+    'pages/numberkeyboard/index',
+    'pages/inputnumber/index',
+    'pages/avatar/index',
     'pages/index/index',
-    'pages/index2/index'
   ],
   window: {
     backgroundTextStyle: 'light',
