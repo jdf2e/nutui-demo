1a429da96f1d05fa88da86423a11cc84d1bf2d5a
suzigang <suzigang@jd.com>
2023-01-07 18:25:34 +0800

    upd: 测试用例提交

---
+++ v4-test/szg2008/taro/src/pages/index/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <nut-cell class="cell">
+      <nut-rate :icon="HeartFill" v-model="value" />
+  </nut-cell>
+</template>
+<script lang="ts">
+import { ref } from 'vue';
+import { HeartFill } from '@nutui/icons-vue-taro';
+export default {
+  components: { HeartFill },
+  setup() {
+      const value = ref(3);
+      return { value, HeartFill }
+  }
+}
+</script>
---
+++ v4-test/szg2008/vite/src/packages/utils/create/index.ts
@@ -0,0 +1,5 @@
+import { createComponent } from "./component";
+import { renderIcon } from "./renderIcon";
+
+export * from "./mountComponent";
+export { createComponent, renderIcon };
