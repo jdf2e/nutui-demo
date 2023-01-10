e77f626d5232e429aff9e8f2a7a5b71508d40b15
yangxiaolu3 <yangxiaolu3@jd.com>
2023-01-10 19:51:21 +0800

    feat: 更新

--- v4-test/yangxiaolu1993/taro/src/pages/index/index.vue
+++ v4-test/yangxiaolu1993/taro/src/pages/index/index.vue
@@ -1,51 +1,108 @@
 <template>
-  <nut-comment
-    headerType="complex"
-    imagesRows="multi"
-    :images="cmt.images"
-    :videos="cmt.videos"
-    :info="cmt.info"
-    ellipsis="6"
-    @clickImages="clickImages"
-  >
-    <template #comment-labels>
-      <img
-        class="nut-comment-header__labels--item"
-        src="https://storage.360buyimg.com/imgtools/78925d9440-f9e874d0-e93d-11eb-8e5c-0da9e18a13b1.png"
-        style="height: 12px"
-      />
-    </template>
-  
-    <template #comment-shop-reply>
-      <div class="nut-comment-shop">
-        <span>京东美妆国际：</span
-        >尊敬的客户您好，非常抱歉给您带来不愉快的购物体验，关于过敏，什么成分都不存在个别性和普遍性。
-      </div>
-    </template>
-  </nut-comment>
-  
-  </template>
-  <script lang="ts">
-  import { reactive, ref,onMounted } from 'vue';
-  import Taro from '@tarojs/taro'
-  export default {
-    setup() {
-      let cmt = ref({});
-      onMounted(()=>{
-        Taro.request({
-            url: 'https://storage.360buyimg.com/nutui/3x/comment_data.json', //仅为示例，并非真实的接口地址
-            success: function (res) {
-              cmt.value = res.data.Comment;
-            }
-          })
-      })
-      const clickImages = (imgs)=>{
-        console.log('进行图片展示',imgs)
+  <nut-cell title="点击查看更多方向" @click="handlePicker"></nut-cell>
+  <nut-popup position="bottom" v-model:visible="showPicker">
+      <nut-picker
+        :columns="columns"
+        title=""
+        @change="change"
+        :swipe-duration="500"
+        @confirm="closePicker"
+        @close="closePicker"
+      >
+        <template #top>
+          <div class="brickBox">
+            <div class="brick" id="pickerTarget"></div>
+          </div>
+        </template>
+      </nut-picker>
+    </nut-popup>
+
+    <nut-popover
+      v-model:visible="customPositon"
+      targetId="pickerTarget"
+      :location="curPostion"
+      theme="dark"
+      :list="positionList"
+    >
+    </nut-popover>
+</template>
+
+<script lang="ts">
+import { reactive, ref } from 'vue';
+export default {
+  setup() {
+    const showPicker = ref(false);
+    const customPositon = ref(false);
+    const curPostion = ref('top');
+    const positionList = reactive([
+      {
+        name: 'option1'
+      },
+      {
+        name: 'option2'
       }
+    ]);
+    const closePicker = () => {
+      customPositon.value = false;
+      showPicker.value = false;
+    };
+
+    const change = ({ selectedValue }) => {
+      curPostion.value = selectedValue[0];
+      if (showPicker.value) customPositon.value = true;
+    };
+
+    const handlePicker = () => {
+      showPicker.value= true;
+      setTimeout(() => {
+        customPositon.value = true;
+      }, 1000);
+    };
+
+     const columns = ref([
+      { text: 'top', value: 'top' },
+      { text: 'top-start', value: 'top-start' },
+      { text: 'top-end', value: 'top-end' },
+      { text: 'right', value: 'right' },
+      { text: 'right-start', value: 'right-start' },
+      { text: 'right-end', value: 'right-end' },
+      { text: 'bottom', value: 'bottom' },
+      { text: 'bottom-start', value: 'bottom-start' },
+      { text: 'bottom-end', value: 'bottom-end' },
+      { text: 'left', value: 'left' },
+      { text: 'left-start', value: 'left-start' },
+      { text: 'left-end', value: 'left-end' }
+    ]);
+
       return {
-        cmt,
-        clickImages
+        positionList,
+        showPicker,
+        customPositon,
+        curPostion,
+        closePicker,
+        change,
+        handlePicker,
+        columns
       };
     }
+};
+</script>
+
+<style lang="scss">
+
+.nut-popover-content {
+    width: 120px;
+}
+
+.brickBox {
+  margin: 80px 0;
+  display: flex;
+  justify-content: center;
+  .brick {
+    width: 60px;
+    height: 60px;
+    background: linear-gradient(135deg, #fa2c19 0%, #fa6419 100%);
+    border-radius: 10px;
   }
-  </script>
+}
+</style>
