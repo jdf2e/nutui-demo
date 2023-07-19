9113d16feb613348f038e8e74b487347afff78ef
Eiinu <eiinu@qq.com>
2023-07-19 11:36:06 +0800

    chore: update v4-vite

--- nutui4-vue/vite-ts/vite.config.ts
+++ nutui4-vue/vite-ts/vite.config.ts
@@ -1,32 +1,24 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import Components from "unplugin-vue-components/vite";
+// vite.config.js
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import NutUIResolver from '@nutui/nutui/dist/resolver'
 
-const NutUIResolver = () => {
-  return (name) => {
-    if (name.startsWith("Nut")) {
-      const partialName = name.slice(3);
-      return {
-        name: partialName,
-        from: "@nutui/nutui",
-        sideEffects: `@nutui/nutui/dist/packages/${partialName.toLowerCase()}/style`,
-      };
-    }
-  };
-};
 // https://vitejs.dev/config/
 export default defineConfig({
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "@nutui/nutui/dist/styles/variables.scss";`,
-      },
-    },
-  },
   plugins: [
     vue(),
+    // 开启 unplugin 插件，自动引入 NutUI 组件
     Components({
       resolvers: [NutUIResolver()],
     }),
   ],
-});
+  // 配置全局样式变量
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@nutui/nutui/dist/styles/variables.scss";`
+      }
+    }
+  }
+})
