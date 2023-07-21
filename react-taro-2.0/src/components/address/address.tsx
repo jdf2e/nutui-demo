459fac89758b3d76954fe010c8141a9729fefc48
wangbei16 <wangbei6@jd.com>
2023-07-21 17:15:32 +0800

    2.0.6

--- react-taro-2.0/src/components/address/address.tsx
+++ react-taro-2.0/src/components/address/address.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { Heart1, HeartFill, Left, Close } from '@nutui/icons-react-taro'
 import Taro from '@tarojs/taro'
 import { Address, Cell } from '@nutui/nutui-react-taro'
@@ -33,6 +33,8 @@ interface T {
   [props: string]: string
 }
 const AddressDemo = () => {
+  const addressRef = useRef<any>(null)
+
   const [translated] = useTranslate<T>({
     'zh-CN': {
       basic: '基础用法',
@@ -45,6 +47,7 @@ const AddressDemo = () => {
       change: '自定义地址与已有地址切换',
       delivery: '配送',
       other: '选择其他地址',
+      uncontrolled: '非受控方式',
     },
     'zh-TW': {
       basic: '基础用法',
@@ -57,6 +60,7 @@ const AddressDemo = () => {
       change: '自定義地址與已有地址切換',
       delivery: '配送',
       other: '選擇其他地址',
+      uncontrolled: '非受控方式',
     },
     'en-US': {
       basic: 'Basic Usage',
@@ -69,11 +73,11 @@ const AddressDemo = () => {
       change: 'Custom Or Exist',
       delivery: 'Delivery',
       other: 'Choose Other Address',
+      uncontrolled: 'Uncontrolled',
     },
   })
 
-  const [value2, setValue2] = useState(['福建', '福州', '台江区'])
-  const [value5, setValue5] = useState(['广东省', '广州市'])
+  const [value2, ] = useState(['福建', '福州', '台江区'])
 
   const [optionsDemo1] = useState([
     {
@@ -224,6 +228,7 @@ const AddressDemo = () => {
     four: translated.title,
     five: translated.title,
     six: translated.title,
+    seven: translated.title,
   })
 
   const [showPopup, setShowPopup] = useState({
@@ -235,14 +240,14 @@ const AddressDemo = () => {
     select: false,
   })
 
-  const [icon, setIcon] = useState({
+  const [icon, ] = useState({
     selectIcon: <HeartFill />,
     defaultIcon: <Heart1 />,
     closeIcon: <Close />,
     backIcon: <Left />,
   })
 
-  const [existList2, setExistList2] = useState([
+  const [existList2, ] = useState([
     {
       id: 1,
       addressDetail: '',
@@ -278,7 +283,7 @@ const AddressDemo = () => {
     },
   ])
 
-  const [existList3, setExistList3] = useState([
+  const [existList3, ] = useState([
     {
       id: 10,
       addressDetail: '',
@@ -314,7 +319,7 @@ const AddressDemo = () => {
     },
   ])
 
-  const [existList4, setExistList4] = useState([
+  const [existList4, ] = useState([
     {
       id: 100,
       addressDetail: '',
@@ -353,7 +358,14 @@ const AddressDemo = () => {
   const showAddress = (tag: string) => {
     setShowPopup({
       ...showPopup,
-      [tag]: !(showPopup as any)[tag],
+      [tag]: true,
+    })
+  }
+
+  const closeAddress = (tag: string) => {
+    setShowPopup({
+      ...showPopup,
+      [tag]: false,
     })
   }
 
@@ -452,6 +464,12 @@ const AddressDemo = () => {
               description={text.four}
               onClick={() => showAddress('other')}
           />
+          <h2>{translated.uncontrolled}</h2>
+          <Cell
+              title={translated.title}
+              description={text.seven}
+              onClick={() => addressRef.current?.open()}
+          />
 
           <Address
               visible={showPopup.normal}
@@ -460,6 +478,7 @@ const AddressDemo = () => {
               onChange={(value, params) => {
                 change1(value, params, 'one')
               }}
+              onClose={() => closeAddress('normal')}
           />
 
           <Address
@@ -474,6 +493,7 @@ const AddressDemo = () => {
               onChange={(value, params) => {
                 change1(value, params, 'six')
               }}
+              onClose={() => closeAddress('select')}
           />
 
           <Address
@@ -484,7 +504,7 @@ const AddressDemo = () => {
               onChange={(value, params) => {
                 change1(value, params, 'five')
               }}
-              // onClose={close5}
+              onClose={() => closeAddress('normal2')}
           />
 
           <Address
@@ -493,6 +513,7 @@ const AddressDemo = () => {
               existList={existList2}
               onExistSelect={selectedTwo}
               title={translated.delivery}
+              onClose={() => closeAddress('exist')}
           />
 
           <Address
@@ -502,7 +523,7 @@ const AddressDemo = () => {
               onExistSelect={selectedThree}
               defaultIcon={icon.defaultIcon}
               selectIcon={icon.selectIcon}
-              // closeIcon={icon.closeIcon}
+              onClose={() => closeAddress('customImg')}
           />
 
           <Address
@@ -519,6 +540,18 @@ const AddressDemo = () => {
               onChange={(value, params) => {
                 change1(value, params, 'four')
               }}
+              onClose={() => closeAddress('other')}
+          />
+
+          <Address
+              ref={addressRef}
+              defaultVisible={false}
+              options={optionsDemo1}
+              title={translated.title}
+              onChange={(value, params) => {
+                change1(value, params, 'one')
+              }}
+              onClose={() => addressRef.current?.close()}
           />
         </div>
       </>
