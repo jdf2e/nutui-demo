0fa00e0217995abbf34a6dbd3914b5f3dc49cac3
eiinu <eiinu@qq.com>
2023-11-06 17:13:31 +0800

    chore: v4.2.0

--- nutui4-vue/taro-tsx/config/index.js
+++ nutui4-vue/taro-tsx/config/index.js
@@ -1,5 +1,5 @@
 import Components from 'unplugin-vue-components/webpack';
-import NutUIResolver from '@nutui/nutui-taro/dist/resolver';
+import NutUIResolver from '@nutui/auto-import-resolver';
 
 const config = {
   projectName: 'taro-jsx',
@@ -34,9 +34,6 @@ const config = {
   }, 
   cache: {
     enable: false // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
-  },
-  sass:{
-    data: `@import "@nutui/nutui-taro/dist/styles/variables.scss";`
   },
   mini: {
     webpackChain(chain) {
